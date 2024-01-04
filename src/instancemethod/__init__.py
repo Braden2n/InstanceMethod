01dbdad3c755e2d5ac58c2a4e40508a143d53e0a
Braden2n <braden@toonetown.com>
2024-01-03 19:04:56 -0700

    Reference links

--- src/instancemethod/__init__.py
+++ src/instancemethod/__init__.py
@@ -5,7 +5,8 @@ AUTHOR = {
     "Name": "Braden Toone",
     "Email": "braden@toonetown.com"
 }
+HOMEPAGE = "https://github.com/Braden2n/InstanceMethod"
 DOCUMENTATION = "https://github.com/Braden2n/InstanceMethod"
 ISSUES = "https://github.com/Braden2n/InstanceMethod/issues"
-
-REPOSITORY = "https://github.com/Braden2n/InstanceMethod"
+REPOSITORY = "https://github.com"
+CHANGELOG = "https://github.com/Braden2n/InstanceMethod/activity"
