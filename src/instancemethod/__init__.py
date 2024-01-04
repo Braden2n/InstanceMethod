0fda54212875bb9c7cb548828672ce3a3a190e20
Braden2n <braden@toonetown.com>
2024-01-03 19:00:02 -0700

    Links and references added

--- src/instancemethod/__init__.py
+++ src/instancemethod/__init__.py
@@ -1,3 +1,9 @@
 __all__ = [
     "decorator",
 ]
+AUTHOR = {
+    "Name": "Braden Toone",
+    "Email": "braden@toonetown.com"
+}
+DOCUMENTATION = "https://github.com/Braden2n/InstanceMethod"
+REPOSITORY = "https://pypi.org/project/instancemethod"
