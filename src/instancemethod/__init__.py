978786323e79ec2a1bbce892a6786d0b13280cb0
Braden2n <braden@toonetown.com>
2024-01-04 01:57:31 -0700

    Added support and testing for nested classes

--- src/instancemethod/__init__.py
+++ src/instancemethod/__init__.py
@@ -83,20 +83,28 @@ def instancemethod(func: Callable[..., Any]) -> Callable[..., Any]:
 
     __________________________________________________________________    
     """
-    def predicate(func: Callable[..., Any]) -> Callable[[object], bool]:
-        """Function generator that returns a predicate function for
-        use with inspect.getmodule.
+    head_caller_name = func.__qualname__.split('.')[0]
+    direct_owner = None
+    def owns_method(obj: object) -> bool:
+        """Recursive function that determines if an object or any of 
+        it's public attributes have ownership over the method.
         """
-        def owns_class(object: object) -> bool:
-            """Predicate function to determine if an object is a class
-            whose name is equal to the owner of a given method.
-            """
-            return (
-                type(object)==type and
-                object.__name__==func.__qualname__[0:-1*(len(func.__name__) + 1)]
-            )
-        return owns_class
+        if hasattr(obj, func.__name__):
+            nonlocal direct_owner
+            direct_owner = obj
+            return True
+        public_attributes = [name for name in dir(obj) if name[0] != '_']
+        for name in public_attributes:
+            attribute = getattr(obj, name)
+            if type(attribute)==type or type(attribute)==object:
+                return owns_method(attribute)
+        return False
     
+    def head_caller(owners: list[tuple[str, object]]) -> object:
+        for owner in owners:
+            if owner[0]==head_caller_name:
+                return owner[1]
+
     def instancemethod_wrapper(*args, **kwargs) -> Any:
         """Function wrapper that determines and verifies caller 
         hierarchy prior to method call.
@@ -107,14 +115,14 @@ def instancemethod(func: Callable[..., Any]) -> Callable[..., Any]:
 
         Raises NotAnInstanceError if not al conditions are met.
         """
-        # Getting the value (index 1) of the first item returned
-        method_owner = getmembers(getmodule(func), predicate(func))[0][1]
+        # Getting the owner (index 1) of the first item returned
+        head_owner = head_caller(getmembers(getmodule(func), owns_method))
         try:
             self = args[0]
         except IndexError:
             pass
         else:
-            if isinstance(self, method_owner):
+            if isinstance(self, head_owner) or isinstance(self, direct_owner):
                 return func(*args, **kwargs)
-        raise NotAnInstanceError(func, method_owner)
+        raise NotAnInstanceError(func, head_owner)
     return instancemethod_wrapper
--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -1,17 +1,26 @@
 import unittest
-import sys
-sys.path.insert(0, "../src/instancemethod.py")
+if __name__ == "__main__":
+    from sys import path
+    path.insert(0, "../src/instancemethod.py")
 from instancemethod import instancemethod, NotAnInstanceError
 
 
 class Class:
     def __init__(self) -> None:
         pass
-    
+
     @instancemethod
     def method(self) -> bool:
         return True
+    
+    class NestedClass:
+        def __init__(self) -> None:
+            pass
 
+        @instancemethod
+        def nested_method(self) -> bool:
+            return True
+    
 
 class SubClass(Class):
     pass
@@ -22,7 +31,8 @@ class AltClass:
         pass
 
 
-TEST_TYPES = [
+TYPES = [
+    None,
     int(),
     float(),
     complex(),
@@ -40,31 +50,42 @@ TEST_TYPES = [
     AltClass(),
 ]
 
-class TestInstanceMethods(unittest.TestCase):
+
+class TestFunctionality(unittest.TestCase):
     def test_class_works(self):
-        instance = Class()
-        self.assertTrue(instance.method())
         self.assertTrue(Class().method())
 
     def test_class_fails(self):
         with self.assertRaises(NotAnInstanceError):
             Class.method()
-        for test_type in TEST_TYPES:
+        for test_type in TYPES:
             with self.assertRaises(NotAnInstanceError):
                 Class.method(test_type)
 
     def test_sub_class_works(self):
-        instance = SubClass()
-        self.assertTrue(instance.method())
         self.assertTrue(SubClass().method())
 
     def test_sub_class_fails(self):
         with self.assertRaises(NotAnInstanceError):
             SubClass.method()
-        for test_type in TEST_TYPES:
+        for test_type in TYPES:
             with self.assertRaises(NotAnInstanceError):
                 SubClass.method(test_type)
 
+    def test_nested_class_works(self):
+        self.assertTrue(Class().NestedClass().nested_method())
+
+    def test_nested_class_fails(self):
+        with self.assertRaises(NotAnInstanceError):
+            Class.NestedClass.nested_method()
+        with self.assertRaises(NotAnInstanceError):
+            Class().NestedClass.nested_method()
+        for test_type in TYPES:
+            with self.assertRaises(NotAnInstanceError):
+                Class.NestedClass.nested_method(test_type)
+            with self.assertRaises(NotAnInstanceError):
+                Class().NestedClass.nested_method(test_type)
+        
 
 if __name__ == "__main__":
     unittest.main()
