683bd6752ed6e9e7dc08d2762d8f9dd9ada0991a
Braden2n <braden@toonetown.com>
2024-01-04 13:58:50 -0700

    5% performance increase by hoisting module loading and replacing owns_method with a lambda function

--- src/instancemethod/__init__.py
+++ src/instancemethod/__init__.py
@@ -82,14 +82,9 @@ def instancemethod(func: Callable[..., Any]) -> Callable[..., Any]:
 
     __________________________________________________________________    
     """
+    # Hoisting declarations to highest order scope for performance
     tree = func.__qualname__.split('.')
-    def owns_method(object: object) -> bool:
-        """Determines if an object owns the method using the name of
-        the first item in the qualified name tree.
-        """
-        nonlocal tree
-        return type(object)==type and object.__name__==tree[0]
-
+    module = getmodule(func)
     def instancemethod_wrapper(*args, **kwargs) -> Any:
         """Function wrapper that determines and verifies caller 
         hierarchy prior to method call.
@@ -101,16 +96,19 @@ def instancemethod(func: Callable[..., Any]) -> Callable[..., Any]:
         Raises NotAnInstanceError if not all conditions are met.
         """
         # Getting the head owner (index 1) of the first item returned
-        owner = getmembers(getmodule(func), owns_method)[0][1]
+            # CANNOT BE HOISTED
+            # Fails if module finishes load before member declaration
+            # Results in IndexError
+        owner = getmembers(
+            module, lambda o : type(o)==type and o.__name__==tree[0]
+        )[0][1]
         # Traversing down attributes to get direct owner
-        for attribute in tree[1:-1]:
-            owner = getattr(owner, attribute)
+        for attribute in tree[1:-1]: owner = getattr(owner, attribute)
         try:
             self = args[0]
         except IndexError:
-            pass
-        else:
-            if isinstance(self, owner):
-                return func(*args, **kwargs)
-        raise NotAnInstanceError(func, owner)
+            raise NotAnInstanceError(func, owner)
+        if not isinstance(self, owner):
+            raise NotAnInstanceError(func, owner)
+        return func(*args, **kwargs)
     return instancemethod_wrapper
