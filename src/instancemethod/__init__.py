8521299723208b1d3de1d7f973b298d8bcee8b62
Braden2n <braden@toonetown.com>
2024-01-03 19:04:45 -0700

    More reference links

--- src/instancemethod/__init__.py
+++ src/instancemethod/__init__.py
@@ -6,4 +6,6 @@ AUTHOR = {
     "Email": "braden@toonetown.com"
 }
 DOCUMENTATION = "https://github.com/Braden2n/InstanceMethod"
-REPOSITORY = "https://pypi.org/project/instancemethod"
+ISSUES = "https://github.com/Braden2n/InstanceMethod/issues"
+
+REPOSITORY = "https://github.com/Braden2n/InstanceMethod"
