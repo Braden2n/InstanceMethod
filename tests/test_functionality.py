9e7e2ad5d7ee220ed1419ad1ad4fb2da0d33032e
Braden2n <braden@toonetown.com>
2024-01-04 00:16:34 -0700

    Added test cases for builtin types passed to self

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -17,10 +17,22 @@ class SubClass(Class):
     pass
 
 
-@instancemethod
-def function() -> bool:
-    return True
-
+TEST_TYPES = [
+    int(),
+    float(),
+    complex(),
+    str(),
+    list(),
+    tuple(),
+    range(0),
+    bytes(),
+    bytearray(),
+    memoryview(bytes(0)),
+    dict(),
+    bool(),
+    set(),
+    frozenset()
+]
 
 class TestInstanceMethods(unittest.TestCase):
     def test_class_works(self):
@@ -31,6 +43,9 @@ class TestInstanceMethods(unittest.TestCase):
     def test_class_fails(self):
         with self.assertRaises(NotAnInstanceError):
             Class.method()
+        for test_type in TEST_TYPES:
+            with self.assertRaises(NotAnInstanceError):
+                Class.method(test_type)
 
     def test_sub_class_works(self):
         instance = SubClass()
@@ -40,6 +55,9 @@ class TestInstanceMethods(unittest.TestCase):
     def test_sub_class_fails(self):
         with self.assertRaises(NotAnInstanceError):
             SubClass.method()
+        for test_type in TEST_TYPES:
+            with self.assertRaises(NotAnInstanceError):
+                SubClass.method(test_type)
 
 
 if __name__ == "__main__":
